f115ac6f0fcdb3a530b7b36a56c505facb5daa53
NNakreSS <87872407+NNakreSS@users.noreply.github.com>
2023-10-13 22:19:15 +0300

    update

--- public/app.js
+++ public/app.js
@@ -4,14 +4,14 @@ window.onload = () => {
   img.onload = () => {
     const colorTake = new TakeColor();
     colorTake.mainColor(img).then((result) => {
-      console.log(result);
       const colorDıv = document.getElementById("dominantColorBox");
       colorDıv.style.background = result;
       colorDıv.style.filter = `drop-shadow(0 0 5px ${result})`;
       document.getElementById("dominantColorRgba").innerHTML = result;
     });
 
-    colorTake.colorPalette(img, 5).then((result) => {
+    colorTake.colorPalette(img, 6, 10).then((result) => {
+      result = result.map((c) => `rgba(${c[0]} , ${c[1]} , ${c[2]})`);
       console.log(result);
       createPalette(result);
     });
@@ -20,7 +20,7 @@ window.onload = () => {
 
 function createPalette(colors) {
   const colorContainer = document.getElementById("colors");
-  for (let index = 0; index < 3; index++) {
+  for (let index = 0; index < 6; index++) {
     const element = colors[index];
     const colorDiv = document.createElement("div");
     colorDiv.className = "colorItem";
--- take_color.js
+++ take_color.js
@@ -8,16 +8,16 @@ class TakeColor {
     return colors[0];
   }
 
-  async colorPalette(img, colorCount = 4) {
-    const colors = Array.from(new Set(await this.#getColors(img)));
-    const colorPalette = [];
-    for (let i = 0; i < colorCount; i++) {
-      colorPalette.push(colors[i]);
-    }
-    return colorPalette;
+  async colorPalette(img, colorCount = 4, quality = 5) {
+    // const colors = Array.from(new Set(await this.#getColors(img, quality)));
+    let colors = this.#getUniqueColors(await this.#getColors(img, quality));
+    // colors = this.#mergeSimilarColors(colors,90);
+    const cmap = MMCQ.quantize(colors, colorCount);
+    const palette = cmap ? cmap.palette() : null;
+    return palette;
   }
 
-  async #getColors(img) {
+  async #getColors(img, quality) {
     this.#canvas.width = img.width;
     this.#canvas.height = img.height;
     this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
@@ -28,29 +28,89 @@ class TakeColor {
       this.#canvas.width,
       this.#canvas.height
     ).data;
-    const imgColorData = await this.#extractColors(imgData);
-    return imgColorData.sort(
-      (a, b) => this.#colorDensisty[b] - this.#colorDensisty[a]
+    const imgColorData = await this.#extractColors(imgData, quality);
+    const sortedColorsData = imgColorData.sort(
+      (a, b) =>
+        this.#colorDensisty[b.join(",")] - this.#colorDensisty[a.join(",")]
     );
+    return sortedColorsData;
   }
 
-  async #extractColors(imgData) {
+  async #extractColors(imgData, quality) {
     const colors = [];
-    imgData.forEach((_v, _i) => {
-      const i = _i * 4;
-      if (
-        imgData[i + 3] > 0 &&
-        imgData[i] != undefined &&
-        imgData[i + 1] != undefined &&
-        imgData[i + 2] != undefined
-      ) {
-        const color = `rgba(
-          ${imgData[i]},${imgData[i + 1]},${imgData[i + 2]},${imgData[i + 3]}
-          )`;
-        this.#colorDensisty[color] = (this.#colorDensisty[color] || 0) + 1;
-        colors.push(color);
+    // imgData.forEach((_v, _i) => {
+    //   _i *= 4;
+    //   const offset = _i * quality;
+    //   const r = imgData[offset],
+    //     g = imgData[offset + 1],
+    //     b = imgData[offset + 2],
+    //     a = imgData[offset + 3];
+    //   if (
+    //     imgData[offset + 3] > 0 &&
+    //     imgData[offset] != undefined &&
+    //     imgData[offset + 1] != undefined &&
+    //     imgData[offset + 2] != undefined
+    //   ) {
+    //     const color = [r, g, b, a];
+    //     this.#colorDensisty[`${r},${g},${b},${a}`] =
+    //       (this.#colorDensisty[`${r},${g},${b},${a}`] || 0) + 1;
+    //     colors.push(color);
+    //   }
+    // });
+    for (let i = 0; i < imgData.length; i += quality) {
+      const offset = i * 4;
+      const r = imgData[offset],
+        g = imgData[offset + 1],
+        b = imgData[offset + 2],
+        a = imgData[offset + 3];
+      if ((typeof a === "undefined" || a >= 125) && typeof b !== "undefined") {
+        // this.#colorDensisty[`${r},${g},${b},${a}`] =
+        this.#colorDensisty[`${r},${g},${b}`] =
+          // (this.#colorDensisty[`${r},${g},${b},${a}`] || 0) + 1;
+          (this.#colorDensisty[`${r},${g},${b}`] || 0) + 1;
+        colors.push([r, g, b]);
       }
-    });
+    }
     return colors;
+  }
+
+  #getUniqueColors(colors) {
+    const uniqueColorsSet = new Set(colors.map((color) => color.toString()));
+    const uniqueColors = Array.from(uniqueColorsSet, (colorStr) =>
+      colorStr.split(",").map(Number)
+    );
+    return uniqueColors;
+  }
+
+  #mergeSimilarColors(colors, threshold = 20) {
+    if (threshold < 20) threshold = 20;
+    const mergedColors = [];
+    for (let i = 0; i < colors.length; i++) {
+      let currentColor = colors[i];
+      let shouldMerge = false;
+      for (let j = 0; j < mergedColors.length; j++) {
+        let mergedColor = mergedColors[j];
+        const colorDifference = Math.sqrt(
+          Math.pow(currentColor[0] - mergedColor[0], 2) +
+            Math.pow(currentColor[1] - mergedColor[1], 2) +
+            Math.pow(currentColor[2] - mergedColor[2], 2)
+          // Math.pow(currentColor[3] - mergedColor[3], 2)
+        );
+        if (colorDifference < threshold) {
+          shouldMerge = true;
+          mergedColors[j] = [
+            Math.round((currentColor[0] + mergedColor[0]) / 2),
+            Math.round((currentColor[1] + mergedColor[1]) / 2),
+            Math.round((currentColor[2] + mergedColor[2]) / 2),
+            // Math.round((currentColor[3] + mergedColor[3]) / 2),
+          ];
+          break;
+        }
+      }
+      if (!shouldMerge) {
+        mergedColors.push(currentColor);
+      }
+    }
+    return mergedColors;
   }
 }
